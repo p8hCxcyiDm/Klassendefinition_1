6c77fd708130a2f52bde763057c0556d5d120b76
p8hCxcyiDm <sam.daellenbach@hotmail.com>
2015-09-19 17:29:09 +0200

    getter added

--- Ticketautomat/src/com/company/Ticketautomat.java
+++ Ticketautomat/src/com/company/Ticketautomat.java
@@ -9,12 +9,27 @@ public class Ticketautomat
     public Ticketautomat(int preis)
     {
         this.preis = preis;
-        this.bisherGezahlt=0;
-        this.gesamtsumme=0;
+        this.bisherGezahlt = 0;
+        this.gesamtsumme = 0;
+    }
+
+    public int getPreis()
+    {
+        return preis;
+    }
+
+    public int getBisherGezahlt()
+    {
+        return bisherGezahlt;
+    }
+
+    public int getGesamtsumme()
+    {
+        return gesamtsumme;
     }
 
     public static void main(String[] args)
     {
-        // write your code here
+
     }
 }
