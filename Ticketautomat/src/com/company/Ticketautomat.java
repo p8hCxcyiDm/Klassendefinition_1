73569ba3e00d1fe1974ff6f3f96903683bf0fe82
p8hCxcyiDm <sam.daellenbach@hotmail.com>
2015-09-19 16:49:25 +0200

    elementary object

---
+++ Ticketautomat/src/com/company/Ticketautomat.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Ticketautomat
+{
+    private int preis;
+    private int bisherGezahlt;
+    private int gesamtsumme;
+
+    public Ticketautomat(int preis)
+    {
+        this.preis = preis;
+        this.bisherGezahlt=0;
+        this.gesamtsumme=0;
+    }
+
+    public static void main(String[] args)
+    {
+        // write your code here
+    }
+}
